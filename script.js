b4ff5195d2f3e1f162bb2be81d3b7bb5eec18c43
aduratovic <aduratovic22@gmail.com>
2022-06-11 20:45:26 -0400

    First Commit

--- script.js
+++ script.js
@@ -20,16 +20,55 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  const redoTitle  = function () {
+    document.getElementById('main-title').innerHTML = 'DOM TIME';
+  };
+  
+  redoTitle();
+ 
   // Part 2
+  const changeBgColor = function () {
+    document.querySelector('body').style.backgroundColor = 'gray';
+  };
 
+  changeBgColor();
+  
   // Part 3
+  const domsFavoriteThings = function() {
+    const lastList = document.getElementById('favorite-things');
+    lastList.removeChild(lastList.lastElementChild)
+  }
+  
+  domsFavoriteThings();
 
   // Part 4
+  const spec = function() {
+    const specTitle = document.querySelectorAll('special-title');
+    for (let title of specTitle) {
+      title.style.fontSize = '2rem';
+    }
+  }
+
+  spec();
 
   // Part 5
+  const races = function() {
+    const pastRaceList = document.getElementById('past-races');
+    pastRaceList.removeChild(pastRaceList.children[3]);
+  };
+
+  races();
 
   // Part 6
+  const newRace = function() {
+    const newCity = document.createElement('li');
+    const pastRaces = document.querySelector('#past-races');
+    newCity.innerHTML = 'Detroit';
+    pastRaces.append(newCity);
+  }
 
+  newRace();
+  
   // Part 7
+  
 }
